0868dc6c545a49ce9cc5c6ab1be2d93cabe74091
PixelatedAxolotl <dontpokethechimera@gmail.com>
2026-03-23 16:40:47 -0400

    customized admin landing page - new tab layout

--- core/views.py
+++ core/views.py
@@ -6,6 +6,13 @@ from .decorators import role_required
 from django.http import JsonResponse
 from accounts.models import School
 
+# Imports to display models on admin dashboard
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import admin
+from accounts.models import User, School, User_Group
+from datasets.models import Dataset
+
+
 
 class CustomLoginView(LoginView):
     template_name = 'core/login.html'
@@ -18,6 +25,17 @@ def school_groups(request, school_id):
         return JsonResponse({'groups': groups})
     except School.DoesNotExist:
         return JsonResponse({'groups': []})
+
+@staff_member_required
+def admin_index(request):
+    context = {
+        **admin.site.each_context(request),
+        'users': User.objects.all(),
+        'schools': School.objects.all(),
+        'user_groups': User_Group.objects.all(),
+        'datasets': Dataset.objects.all(),
+    }
+    return render(request, 'admin/index.html', context)
 
 
 @role_required('internal', 'school_admin')
